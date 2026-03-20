bf9f7faa260205cd3064accf458644f7d3cca849
jasper <2190320413@qq.com>
2026-03-20 22:35:09 +0800

    enum

---
+++ foundation/src/main/java/com/jasper/dataType/enums/ColorEnum.java
@@ -0,0 +1,37 @@
+package com.jasper.dataType.enums;
+
+public enum ColorEnum {
+
+    // 枚举常量默认是 public static final 的
+    // public static final com.jasper.dataType.enums.ColorEnum RED;
+    // public static final com.jasper.dataType.enums.ColorEnum GREEN;
+    // public static final com.jasper.dataType.enums.ColorEnum BLUE;
+    RED("红色") {
+        @Override
+        public String getColor() {
+            return "red";
+        }
+    },
+    GREEN("绿色") {
+        @Override
+        public String getColor() {
+            return "green";
+        }
+    },
+    BLUE("蓝色") {
+        @Override
+        public String getColor() {
+            return "blue";
+        }
+    };
+
+    private String color;
+
+    private ColorEnum(String color) {
+        this.color = color;
+    }
+
+    // 抽象方法，所有枚举常量必须实现该方法
+    public abstract String getColor();
+
+}
---
+++ foundation/src/main/java/com/jasper/dataType/enums/MainDemo.java
@@ -0,0 +1,14 @@
+package com.jasper.dataType.enums;
+
+public class MainDemo {
+    public static void main(String[] args) {
+        for (ColorEnum enum1 : ColorEnum.values()) {
+            System.out.println(enum1);
+        }
+        System.out.println(ColorEnum.RED.name());
+        // ordinal 返回枚举常量的“下标（序号）”
+        System.out.println(ColorEnum.RED.ordinal());
+        // vlaueof 返回指定字符串的枚举常量 字符串 → 枚举
+        System.out.println(ColorEnum.valueOf("GREEN"));
+    }
+}
