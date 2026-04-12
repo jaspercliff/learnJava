af3092396ccdf06a7ce2c9f88e98879b35cde59f
jasper <2190320413@qq.com>
2026-04-12 11:25:16 +0800

    format

--- basic/foundation/src/main/java/jasper/dataType/StringInternDemo.java
+++ basic/foundation/src/main/java/jasper/dataType/StringInternDemo.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class StringInternDemo {
     public static void main(String[] args) {
         // 模拟 30 个固定的城市名
-        String[] cities = {"北京市", "上海市", "广州市", "深圳市", "杭州市", /*...省略其余25个*/ "成都市"};
+        String[] cities = {"北京市", "上海市", "广州市", "深圳市", "杭州市", /* ...省略其余25个 */ "成都市"};
         List<String> resultList = new ArrayList<>();
 
         int count = 1000000; // 100 万行数据
@@ -27,7 +27,8 @@ public class StringInternDemo {
 
             // 方案 B：使用 intern（大获全胜）
             resultList.add(rawCity.intern());
-//            堆内存里只有 30 个 String 对象 被 resultList 引用。剩下的 999,970 个临时产生的 rawCity 对象因为没有被引用，会被 GC 迅速回收
+            // 堆内存里只有 30 个 String 对象 被 resultList 引用。剩下的 999,970 个临时产生的 rawCity 对象因为没有被引用，会被
+            // GC 迅速回收
         }
     }
 }
