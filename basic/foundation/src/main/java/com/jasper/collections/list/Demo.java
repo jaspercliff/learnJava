94a60f80b83ebc403a76e17f0ae7fc4854c35f36
jasper <2190320413@qq.com>
2026-05-15 16:20:41 +0800

    arraylist

--- basic/foundation/src/main/java/com/jasper/collections/list/Demo.java
+++ basic/foundation/src/main/java/com/jasper/collections/list/Demo.java
@@ -1,10 +1,13 @@
 package com.jasper.collections.list;
 
+import java.util.ArrayList;
 
 /**
  * @author jasper
  * @since 2026-04-28 10:02:52
  */
 public class Demo {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        ArrayList<Integer> lsit = new ArrayList<>();
+    }
 }
